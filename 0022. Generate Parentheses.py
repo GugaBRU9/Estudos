4e1a5505aec15c05f213190978e5e20f95fd36ce
GugaBRU9 <gugabru13@gmail.com>
2025-05-23 11:14:50 -0300

    Adding solvo of LeetCode 22

---
+++ 0022. Generate Parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def backtrack(s, o, c):
+            if len(s) == 2 * n:
+                res.append(s)
+                return
+            else:
+                if o < n:
+                    backtrack(s + '(', o + 1, c)
+                if c < o:
+                    backtrack(s + ')', o, c + 1)
+        res = []
+        backtrack('', 0, 0)
+        return list(res)
